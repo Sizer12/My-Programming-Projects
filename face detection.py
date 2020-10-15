d64e70001fc12d6056b813df1c16a4e9d2461d56
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2020-10-15 21:36:39 +0300

    Update face detection.py

--- face detection.py
+++ face detection.py
@@ -9,12 +9,13 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
-
-while 1:
+def readFaces():
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
+    
+while 1:
+    readFaces()
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         print('x: '+ x + ' y: '+ y)
